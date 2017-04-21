7c7e99857eac31d75ac9d1029c730526c7a353a0
Michael Morasch <mmorasch@techfak.uni-bielefeld.de>
2017-04-21 11:30:17 +0200

    made schema according to API documentation

--- server/schema.sql
+++ server/schema.sql
@@ -1,8 +1,41 @@
-create table _user (
-  userId integer primary key autoincrement,
-  email text not null,
-  password text not null,
-  firstName text primary key not null,
-  lastName text primary key not null,
-  profilePictureURL text not null
+create table server.user (
+    userId INTEGER PRIMARY KEY autoincrement,
+    email TEXT NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    firstName TEXT PRIMARY KEY NOT NULL,
+    lastName TEXT PRIMARY KEY NOT NULL,
+    profilePictureURL TEXT
+);
+
+create table server.activity (
+    activityId INTEGER PRIMARY KEY autoincrement,
+    name TEXT NOT NULL,
+    maximumUsers INTEGER DEFAULT = 0
+);
+
+create table server.room (
+    roomId INTEGER PRIMARY KEY autoincrement,
+    name TEXT NOT NULL,
+    maximumUsers INTEGER DEFAULT = 0
+);
+
+create table server.breakSeekerRequest (
+    breakId INTEGER PRIMARY KEY,
+    server.userId  INTEGER NOT NULL FOREIGN KEY,
+    joined TEXT NOT NULL,
+    status TEXT NOT NULL,
+    activities BLOB NOT NULL
+);
+
+create table server.breakMatch (
+    partners BLOB NOT NULL,
+    server.activityId INTEGER NOT NULL FOREIGN KEY,
+    server.roomId INTEGER NOT NULL FOREIGN KEY,
+    _timestamp CURRENT_TIMESTAMP
+);
+
+create table server.apiResponse (
+    code INTEGER NOT NULL,
+    type TEXT NOT NULL,
+    message TEXT NOT NULL
 );
