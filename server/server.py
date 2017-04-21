c7e8e9e8345cda2fead7dc470f5ae04bee8acd7e
Michael Morasch <mmorasch@techfak.uni-bielefeld.de>
2017-04-21 23:19:50 +0200

    configured database connection from main program

--- server/server.py
+++ server/server.py
@@ -3,9 +3,18 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
 app = Flask(__name__)
+app.config.from_object(__name__)
+
+
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'server.db'),
+    DATABASE=os.path.join(app.root_path, 'interactive_office.db'),
     SECRET_KEY='development key',
     USERNAME='admin',
     PASSWORD='default'
 ))
+
+
+def connect_db():
+    rv = sqlite3.connect(app.config['DATABASE'])
+    rv.row_factory = sqlite3.Row
+    return rv
