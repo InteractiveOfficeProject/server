caf37aaad0aa5b365979af6a630ce1079ca95cd2
Jan Lippert <lippertsjan@gmail.com>
2017-05-09 13:27:44 +0300

    Added some utility scripts

---
+++ server/create_default_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO user (email, firstName, lastName) VALUES 
+  ("jan@iop.com", "Jan", "Lippert"),
+  ("peter@iop.com", "Péter", "Ivanics"),
+  ("michael@iop.com", "Michael", "Morrasch");
+
+INSERT INTO activity (name, maximumUsers) VALUES
+  ("Coffee", 0),
+  ("Walking outside", 0),
+  ("Talking with someone", 0),
+  ("Eating something", 0),
+  ("Playing a game", 0);
